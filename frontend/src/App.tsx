053640aa5f0aa8a06183b0d56622000005e022f3
git-spec <inkyfischer@gmail.com>
2025-05-26 22:31:22 +0200

    chore: Created global theme for App.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
+import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import {ThemeProvider} from '@mui/material/styles';
+
+import {globalTheme} from "./themes/globalTheme.ts";
+
 
 function App() {
 
   return (
     <>
-      <p style={{textAlign: 'center', marginTop: '8rem'}}>
-        Hello World!
-      </p>
+      <ThemeProvider theme={globalTheme}>
+          <CssBaseline>
+              <p style={{textAlign: 'center', marginTop: '8rem', fontFamily: 'SourceSerif4', fontWeight: 200, fontStyle: 'normal', fontSize: 24, letterSpacing: '.03em'}}>
+                  Hello World!
+              </p>
+          </CssBaseline>
+      </ThemeProvider>
     </>
   )
 }
---
+++ frontend/src/themes/globalTheme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+export const globalTheme = createTheme({
+    typography: {
+        fontFamily: 'SourceSerif4, SansSans3',
+    },
+    components: {
+      //   MuiCssBaseline: {
+      //       styleOverrides: `
+      //   p {
+      //     color: grey;
+      //   }
+      // `,
+      //   },
+    },
+});
