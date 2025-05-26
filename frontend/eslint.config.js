c2e107f79349cf1508ae3e1024d6cbc99852fd16
git-spec <inkyfischer@gmail.com>
2025-05-26 09:45:46 +0200

    chore: Added teslint to eslint.config.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,18 +7,23 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules, 
+      ...tseslint.configs.stylisticTypeChecked,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
