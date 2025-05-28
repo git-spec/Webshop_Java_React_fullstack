7fa38869f526ba1083feb5e62c67756b75305708
git-spec <inkyfischer@gmail.com>
2025-05-28 10:30:12 +0200

    test: Created ArticleControllerTest

--- backend/src/main/java/org/example/backend/repository/ArticleRepo.java
+++ backend/src/main/java/org/example/backend/repository/ArticleRepo.java
@@ -2,9 +2,11 @@ package org.example.backend.repository;
 
 import org.example.backend.model.Article;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
 
+@Repository
 public interface ArticleRepo extends MongoRepository<Article, String> {
     List<Article> findAllByProductCategory(String category);
     List<Article> findAllByProductGroup(String group);
