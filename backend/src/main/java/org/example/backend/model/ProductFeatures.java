dcfac86d48c16bc58c0479e5872c30125cb788a4
git-spec <inkyfischer@gmail.com>
2025-05-28 08:56:03 +0200

    rebase: Changed colors to list in ProductFeatures

--- backend/src/main/java/org/example/backend/model/ProductFeatures.java
+++ backend/src/main/java/org/example/backend/model/ProductFeatures.java
@@ -1,8 +1,10 @@
 package org.example.backend.model;
 
+import java.util.List;
+
 
 public record ProductFeatures(   
     Dimension dimension,
     Measure weight,
-    Colors colors
+    List<Color> colors
 ) {}
