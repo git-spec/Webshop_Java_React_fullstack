7a63b864d75dde74229b18e1a6a45fdea742381f
git-spec <inkyfischer@gmail.com>
2025-05-30 15:53:20 +0200

    feat: Added routes to App.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,22 @@
-import './App.css';
-import CssBaseline from '@mui/material/CssBaseline';
-import {ThemeProvider} from '@mui/material/styles';
+import {Routes, Route} from "react-router-dom";
 
-import {globalTheme} from "./themes/globalTheme.ts";
+import './App.css';
+import Products from "./page/Products.tsx";
 
 
 function App() {
 
   return (
     <>
-      <ThemeProvider theme={globalTheme}>
-          <CssBaseline>
-              <p style={{textAlign: 'center', marginTop: '8rem', fontFamily: 'SourceSerif4', fontWeight: 200, fontStyle: 'normal', fontSize: 24, letterSpacing: '.03em'}}>
-                  Hello World!
-              </p>
-          </CssBaseline>
-      </ThemeProvider>
+      <p style={{textAlign: 'center', marginTop: '8rem', fontFamily: 'SourceSerif4', fontWeight: 200, fontStyle: 'normal', fontSize: 24, letterSpacing: '.03em'}}>
+          Hello World!
+      </p>
+      <Routes>
+            <Route path={'/products/:categotry'} element={<Products />} />
+            <Route path={'/products/:group'} element={<Products />} />
+            <Route path={'/products/:family'} element={<Products />} />
+            <Route path="/products" element={<Products />} />
+      </Routes>
     </>
   )
 }
