f08f78f595c9bcdedb443fe3c8fb79748917bc23
git-spec <inkyfischer@gmail.com>
2025-05-30 17:54:05 +0200

    feat: Created getProductsByGroup in ProductService.

--- backend/src/main/java/org/example/backend/service/ProductService.java
+++ backend/src/main/java/org/example/backend/service/ProductService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.example.backend.model.Product;
 import org.example.backend.model.Category;
+import org.example.backend.model.Group;
 import org.example.backend.repository.ProductRepo;
 import org.example.backend.exception.CategoryNotFoundException;
 
@@ -16,6 +17,7 @@ public class ProductService {
     private final ProductRepo productRepo;
 
     private static final String CATEGORY_NOT_FOUND_MESSAGE_FORMAT = "Kategorie %s existiert nicht.";
+    private static final String GROUP_NOT_FOUND_MESSAGE_FORMAT = "KGruppe %s existiert nicht.";
 
     public List<Product> getProducts() {
         return productRepo.findAll();
@@ -32,6 +34,20 @@ public class ProductService {
             return productRepo.findAllByCategory(categoryEnum.toString());
         } catch (Exception e) {
             throw new CategoryNotFoundException(CATEGORY_NOT_FOUND_MESSAGE_FORMAT, category);
+        }
+    }
+
+    /**
+     * Gets products by group.
+     * @param group
+     */
+    public List<Product> getProductsByGroup(String group) {
+        // Gets enum of string.
+        try {
+            Group groupEnum = Group.valueOf(group.toUpperCase());
+            return productRepo.findAllByGroup(groupEnum.toString());
+        } catch (Exception e) {
+            throw new CategoryNotFoundException(GROUP_NOT_FOUND_MESSAGE_FORMAT, group);
         }
     }
 }
