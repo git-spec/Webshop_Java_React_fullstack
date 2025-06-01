79f2c3fa048e06c054929f983330a9428343c343
git-spec <inkyfischer@gmail.com>
2025-06-01 11:13:32 +0200

    feat: Created getProductsByCategoryAndGroup in ProductController.

--- backend/src/main/java/org/example/backend/controller/ProductController.java
+++ backend/src/main/java/org/example/backend/controller/ProductController.java
@@ -2,19 +2,25 @@ package org.example.backend.controller;
 
 import java.util.List;
 
+import org.example.backend.exception.NotFoundException;
+import org.example.backend.model.Product;
+import org.example.backend.model.dto.ErrorDTO;
+import org.example.backend.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import org.example.backend.service.ProductService;
-import org.example.backend.model.Product;
 
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
     private final ProductService productService;
+
+    private static final String NOT_FOUND_MESSAGE_FORMAT = "Seite nicht gefunden.";
 
     public ProductController(ProductService productService) {
         this.productService = productService;
@@ -28,5 +34,23 @@ public class ProductController {
     @GetMapping("/products/{category}")
     public List<Product> getProductsByCategory(@PathVariable String category) {
         return productService.getProductsByCategory(category);
+    }
+
+    @GetMapping("/products/{category}/{group}")
+    public List<Product> getProductsByCategoryAndGroup(@PathVariable String category, @PathVariable String group) {
+        return productService.getProductsByCategoryAndGroup(category, group);
+    }
+
+    /**
+     * Throws NOT_FOUND Exception if path variable is invalid.
+     * @param IllegalArgumentException
+     * @throws NotFoundException
+     */
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorDTO> illegalArgumentExceptionHandler(IllegalArgumentException exception) {
+        return new ResponseEntity<>(
+                ErrorDTO.fromException(new NotFoundException(NOT_FOUND_MESSAGE_FORMAT, HttpStatus.NOT_FOUND.name())),
+                HttpStatus.NOT_FOUND
+            );
     }
 }
