0fd8cfa6b374695b218161a2d1050ee55c924aea
git-spec <inkyfischer@gmail.com>
2025-06-02 17:24:47 +0200

    refactor: Added throwables to requests of ProductService.

--- backend/src/main/java/org/example/backend/service/ProductService.java
+++ backend/src/main/java/org/example/backend/service/ProductService.java
@@ -1,19 +1,27 @@
 package org.example.backend.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import lombok.RequiredArgsConstructor;
 import java.util.List;
 
 import org.example.backend.model.Product;
+import org.example.backend.model.dto.ErrorDTO;
 import org.example.backend.model.Category;
 import org.example.backend.model.Group;
 import org.example.backend.repository.ProductRepo;
+import org.example.backend.exception.NotFoundException;
 
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepo productRepo;
+
+    private static final String NOT_FOUND_MESSAGE_FORMAT = "Seite nicht gefunden.";
 
     public List<Product> getProducts() {
         return productRepo.findAll();
@@ -22,23 +30,31 @@ public class ProductService {
     /**
      * Gets products by category.
      * @param category
-     * @throws IllegalArgumentException 
+     * @throws NotFoundException
      */
-    public List<Product> getProductsByCategory(String category) {
+    public List<Product> getProductsByCategory(String category) throws NotFoundException {
         // Gets enum of string.
-        Category categoryEnum = Category.valueOf(category.toUpperCase());
-        return productRepo.findAllByCategory(categoryEnum.toString());
+        try {
+            Category categoryEnum = Category.valueOf(category.toUpperCase());
+            return productRepo.findAllByCategory(categoryEnum.toString());
+        } catch(IllegalArgumentException e) {
+            throw new NotFoundException(NOT_FOUND_MESSAGE_FORMAT, e);
+        }
     }
 
     /**
      * Gets products by group.
      * @param group
-     * @throws IllegalArgumentException 
+     * @throws NotFoundException 
      */
-    public List<Product> getProductsByCategoryAndGroup(String category, String group) {
+    public List<Product> getProductsByCategoryAndGroup(String category, String group) throws NotFoundException {
         // Gets enum of string.
-        Category categoryEnum = Category.valueOf(category.toUpperCase());
-        Group groupEnum = Group.valueOf(group.toUpperCase());
-        return productRepo.findByCategoryAndGroup(categoryEnum.toString(), groupEnum.toString());
+        try {
+            Category categoryEnum = Category.valueOf(category.toUpperCase());
+            Group groupEnum = Group.valueOf(group.toUpperCase());
+            return productRepo.findAllByCategoryAndGroup(categoryEnum.toString(), groupEnum.toString());
+        } catch(IllegalArgumentException e) {
+            throw new NotFoundException(NOT_FOUND_MESSAGE_FORMAT, e);
+        }
     }
 }
