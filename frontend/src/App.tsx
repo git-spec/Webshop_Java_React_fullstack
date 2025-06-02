5885605f0e9b2bc1e11d1290cd562405032e1235
git-spec <inkyfischer@gmail.com>
2025-06-02 18:40:56 +0200

    feat: Added RootLayout and changed routes.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,21 @@ import {Routes, Route} from "react-router-dom";
 
 import './App.css';
 import Products from "./page/Products.tsx";
+import RootLayout from "./component/layout/RootLayout.tsx";
 
 
 function App() {
 
   return (
-    <>
-      <p style={{textAlign: 'center', marginTop: '8rem', fontFamily: 'SourceSerif4', fontWeight: 200, fontStyle: 'normal', fontSize: 24, letterSpacing: '.03em'}}>
-          Hello World!
-      </p>
-      <Routes>
-            <Route path={'/products/:category/:group/:family'} element={<Products />} />
-            <Route path={'/products/:category/:group'} element={<Products />} />
-            <Route path={'/products/:category'} element={<Products />} />
-            <Route path="/products" element={<Products />} />
-      </Routes>
-    </>
+    <Routes>
+      {/*APP RootLayout*/}
+      <Route element={<RootLayout />}>
+        <Route path={'/products/:category/:group/:family'} element={<Products />} />
+        <Route path={'/products/:category/:group'} element={<Products />} />
+        <Route path={'/products/:category'} element={<Products />} />
+        <Route path="/products" element={<Products />} />
+      </Route>
+    </Routes>
   )
 }
 
