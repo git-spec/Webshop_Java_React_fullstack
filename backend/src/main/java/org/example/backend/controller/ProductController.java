8eac146083038c0684b2051ecd58d53be70e929c
git-spec <inkyfischer@gmail.com>
2025-06-02 17:25:04 +0200

    refactor: Added throwables to requests of ProductController.

--- backend/src/main/java/org/example/backend/controller/ProductController.java
+++ backend/src/main/java/org/example/backend/controller/ProductController.java
@@ -1,11 +1,6 @@
 package org.example.backend.controller;
 
 import java.util.List;
-
-import org.example.backend.exception.NotFoundException;
-import org.example.backend.model.Product;
-import org.example.backend.model.dto.ErrorDTO;
-import org.example.backend.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.example.backend.exception.NotFoundException;
+import org.example.backend.model.Product;
+import org.example.backend.model.dto.ErrorDTO;
+import org.example.backend.service.ProductService;
 
 
 @RestController
@@ -32,12 +32,12 @@ public class ProductController {
     }
 
     @GetMapping("/products/{category}")
-    public List<Product> getProductsByCategory(@PathVariable String category) {
+    public List<Product> getProductsByCategory(@PathVariable String category) throws NotFoundException {
         return productService.getProductsByCategory(category);
     }
 
     @GetMapping("/products/{category}/{group}")
-    public List<Product> getProductsByCategoryAndGroup(@PathVariable String category, @PathVariable String group) {
+    public List<Product> getProductsByCategoryAndGroup(@PathVariable String category, @PathVariable String group) throws NotFoundException {
         return productService.getProductsByCategoryAndGroup(category, group);
     }
 
@@ -49,7 +49,7 @@ public class ProductController {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorDTO> illegalArgumentExceptionHandler(IllegalArgumentException exception) {
         return new ResponseEntity<>(
-                ErrorDTO.fromException(new NotFoundException(NOT_FOUND_MESSAGE_FORMAT, HttpStatus.NOT_FOUND.name())),
+                ErrorDTO.fromException(new NotFoundException(NOT_FOUND_MESSAGE_FORMAT, exception)),
                 HttpStatus.NOT_FOUND
             );
     }
