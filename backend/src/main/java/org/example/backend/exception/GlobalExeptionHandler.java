a60240aae66ab19048990812c28602a65411f214
git-spec <inkyfischer@gmail.com>
2025-06-02 15:16:04 +0200

    fix: Deleted ArgumentNotFoundException.

--- backend/src/main/java/org/example/backend/exception/GlobalExeptionHandler.java
+++ backend/src/main/java/org/example/backend/exception/GlobalExeptionHandler.java
@@ -1,13 +1,10 @@
 package org.example.backend.exception;
 
+import org.example.backend.model.dto.ErrorDTO;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import org.example.backend.model.dto.ErrorDTO;
-import org.example.backend.exception.NotFoundException;
 
 @RestControllerAdvice
 public class GlobalExeptionHandler {
@@ -21,11 +18,5 @@ public class GlobalExeptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorDTO handleNotFound(NotFoundException e) {
         return ErrorDTO.fromException(e).withStatus(HttpStatus.NOT_FOUND.name());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleArgumentNotFoundException(ArgumentNotFoundException e) {
-        return e.getMessage();
     }
 }
