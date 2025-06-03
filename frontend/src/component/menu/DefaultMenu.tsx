1828fa58fdfc0b14525ceb8fc118168b8c5d6c53
git-spec <inkyfischer@gmail.com>
2025-06-03 14:34:56 +0200

    feat: Created DefaultMenu for account.

---
+++ frontend/src/component/menu/DefaultMenu.tsx
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
+type Props = {
+    id: string;
+    anchorEl: HTMLElement | null;
+    onClose: (close: null) => void;
+};
+
+function DefaultMenu({id, anchorEl, onClose}: Readonly<Props>) {
+    let anchor: null | HTMLElement = null;
+    const menuId = id;
+    const isOpen = Boolean(anchorEl);
+
+    useEffect(() => {
+        console.log(anchorEl);
+        
+        if (anchorEl) {
+            anchor = anchorEl;
+        }
+    }, [anchorEl]);
+
+    const handleMenuClose = () => {
+        anchor = null;
+        onClose(null);
+    };
+
+    return (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        </Menu>
+    );
+}
+
+export default DefaultMenu;
