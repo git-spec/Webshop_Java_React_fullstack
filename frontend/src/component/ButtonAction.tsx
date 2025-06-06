b2921d76d3cdf48a9fbd236d4c3922906a584e5d
git-spec <inkyfischer@gmail.com>
2025-06-06 16:29:45 +0200

    feat: Created component ButtonAction.

---
+++ frontend/src/component/ButtonAction.tsx
@@ -0,0 +1,35 @@
+import Button from "@mui/material/Button";
+import type { ReactNode } from "react";
+
+type Props = {
+    key?: string | number;
+    value: string | ReactNode;
+    color?: "inherit" | "primary" | "secondary" | "success" | "error" | "info" | "warning";
+    variant?: "outlined" | "text" | "contained";
+    disableRipple?: boolean; 
+    click?: () => void;
+    href?: string;
+};
+
+export default function ButtonAction(props: Readonly<Props>) {
+
+    return (
+        <>
+            {
+                props.click && 
+                    <Button 
+                        key={props.key ?? ''}
+                        variant={props.variant ?? 'outlined'} 
+                        size="small" 
+                        color={props.color} 
+                        sx={{fontFamily: 'SourceSans3'}} 
+                        onClick={props.click}
+                        disableRipple={props.disableRipple ?? false}
+                    >
+                        {props.value}
+                    </Button>
+            }
+            {props.href && <Button variant={props.variant ?? 'outlined'} size="small" sx={{fontFamily: 'SourceSans3'}} href={props.href}>{props.value}</Button>}
+        </>
+    );
+}
