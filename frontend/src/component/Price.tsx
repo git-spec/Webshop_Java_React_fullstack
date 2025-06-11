a13c4162682e759dab5e96ca408af7f30a353706
git-spec <inkyfischer@gmail.com>
2025-06-11 07:47:28 +0200

    refactor: Added props to Price.

--- frontend/src/component/Price.tsx
+++ frontend/src/component/Price.tsx
@@ -8,11 +8,23 @@ type Props = {
     currency: string;
     justify: string;
     fontWeight?: number;
+    fontSize?: string;
+    fontStyle?: string;
     color?: string;
 };
 
 
-export default function Price({value, currency, justify, fontWeight, color}: Readonly<Props>) {
+export default function Price(
+    {
+        value, 
+        currency, 
+        justify, 
+        fontWeight, 
+        fontSize, 
+        fontStyle, 
+        color
+    }: Readonly<Props>
+) {
     return (
         <Stack direction={'row'} sx={{justifyContent: `${justify}`, color: `${color ?? 'black'}`}}>
             <Typography
@@ -22,7 +34,9 @@ export default function Price({value, currency, justify, fontWeight, color}: Rea
                 sx={
                     {
                         paddingTop: '2px',
-                        fontWeight: `${fontWeight ?? 300}`
+                        fontWeight: `${fontWeight ?? 300}`,
+                        fontSize: `${fontSize ?? 'inherit'}`,
+                        fontStyle: `${fontStyle ?? 'inherit'}`
                     }
                 }
             >
@@ -36,7 +50,9 @@ export default function Price({value, currency, justify, fontWeight, color}: Rea
                         {
                             paddingTop: '2px',
                             marginLeft: '.3rem',
-                            fontWeight: `${fontWeight ?? 300}`
+                            fontWeight: `${fontWeight ?? 300}`,
+                            fontSize: `${fontSize ?? 'inherit'}`,
+                            fontStyle: `${fontStyle ?? 'inherit'}`
                         }
                     }
             >
