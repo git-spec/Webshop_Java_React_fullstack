63e301ac2c5ee9a9381ad99b79d407f9bcf215bf
git-spec <inkyfischer@gmail.com>
2025-06-11 13:58:02 +0200

    style: Adjusted grid of Checkout.

---
+++ frontend/src/page/Checkout.tsx
@@ -0,0 +1,59 @@
+import { useLocation } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+
+import type { ICart } from "@/interface/ICart";
+import Order from "@/component/share/Order";
+import LayoutContainer from '@/component/share/LayoutContainer';
+import AccordionExpand from '@/component/AccordionExpand';
+import PersonDetailsForm from '@/component/PersonDetailsForm';
+import type { FormEvent } from 'react';
+import TextField from '@mui/material/TextField';
+import ButtonAction from '@/component/ButtonAction';
+import PayPal from '@/component/PayPal';
+
+
+export default function Checkout() {
+    const location = useLocation();
+    
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {};
+
+    const Accs = [
+        {
+            id: 'contact',
+            sumary: 'Kontakt',
+            Component: <TextField label={'E-Mail-Adresse'} size={'small'} fullWidth />
+        },
+        {
+            id: 'address',
+            sumary: 'Adresse',
+            Component: PersonDetailsForm
+        },
+        {
+            id: 'payment',
+            sumary: 'Zahlung',
+            Component: PayPal
+        }
+    ];
+
+    return (
+        <LayoutContainer>
+            <form onSubmit={handleSubmit}>
+                <Grid container spacing={4}>
+                    <Grid size={7}>
+                        {
+                            Accs.map(acc => <AccordionExpand key={acc.id} sumary={acc.sumary} Component={acc.Component} />)
+                        }                    
+                    </Grid>
+                    <Grid size={5}>  
+                        <Grid size={12} sx={{mb: 2}}>             
+                            <Order orders={location.state} checkout={true} />
+                        </Grid> 
+                        <Grid size={12}>
+                            <ButtonAction type='submit' value={'kaufen'} color="success" fitContent={false}  />
+                        </Grid>
+                    </Grid>
+                </Grid>
+        </form>
+        </LayoutContainer>
+    );
+}
