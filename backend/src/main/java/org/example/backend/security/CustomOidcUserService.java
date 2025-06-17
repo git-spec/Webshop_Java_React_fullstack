11c3fd15c664d7c7029c1aa5c823ba282d87eb5e
git-spec <inkyfischer@gmail.com>
2025-06-17 08:15:11 +0200

    feat: Created CustomOidcUserService.

---
+++ backend/src/main/java/org/example/backend/security/CustomOidcUserService.java
@@ -0,0 +1,44 @@
+package org.example.backend.security;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.example.backend.model.User;
+import org.example.backend.repository.UserRepo;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomOidcUserService extends OidcUserService {
+    private final UserRepo userRepo;
+
+    @Override
+    public OidcUser loadUser(OidcUserRequest  userRequest) throws OAuth2AuthenticationException {
+        OidcUser oidcUser = super.loadUser(userRequest);
+        @SuppressWarnings("unused")
+        User user = userRepo.findById(oidcUser.getName())
+            .orElseGet(() -> createUser(oidcUser));
+
+        return oidcUser;
+    }
+
+    private User createUser(OidcUser oidcUser) {
+        Map<String, Object> userMap = oidcUser.getAttributes();
+        User newUser = new User(
+            (String) userMap.get("sub"),
+            (String) userMap.get("email"),
+            (String) userMap.get("given_name"),
+            (String) userMap.get("family_name")
+        );
+        
+        userRepo.save(newUser);
+        return newUser;
+    }
+}
