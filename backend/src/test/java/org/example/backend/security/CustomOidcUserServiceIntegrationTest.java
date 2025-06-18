7959fdce232892b844f0fbd65d6cedd74015d7be
git-spec <inkyfischer@gmail.com>
2025-06-18 18:07:48 +0200

    style: Deleted unused imports of CustomOidcUserServiceIntegrationTest.

--- backend/src/test/java/org/example/backend/security/CustomOidcUserServiceIntegrationTest.java
+++ backend/src/test/java/org/example/backend/security/CustomOidcUserServiceIntegrationTest.java
@@ -1,9 +1,6 @@
 package org.example.backend.security;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
