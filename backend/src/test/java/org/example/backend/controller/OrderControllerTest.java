118bf28af8bcc6e164473f5c9330e2cea7be2c49
git-spec <inkyfischer@gmail.com>
2025-06-18 16:08:08 +0200

    test: Created tests for getOrdersByEmail in OrderControllerTest.

--- backend/src/test/java/org/example/backend/controller/OrderControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/OrderControllerTest.java
@@ -1,10 +1,14 @@
 package org.example.backend.controller;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -14,6 +18,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import java.math.BigDecimal;
 
+import org.example.backend.exception.EntriesNotFoundException;
 import org.example.backend.model.Article;
 import org.example.backend.model.OrderCompleted;
 import org.example.backend.model.PayPal;
@@ -22,6 +27,7 @@ import org.example.backend.repository.OrderRepo;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class OrderControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -39,9 +45,32 @@ public class OrderControllerTest {
         OrderCompleted order = new OrderCompleted("123", articles, paypal);
         orderRepo.save(order);
         // WHEN // THEN
-        mockMvc.perform(get("/api/orders/" + email))
+        mockMvc.perform(get("/api/orders/{email}", email))
             .andExpect(status().isOk())
             .andExpect(content().json(String.valueOf(objectMapper.writeValueAsString(List.of(order)))));
+    }
+
+    @Test
+    void getOrdersByEmail_shouldThrowBadRequest_whenGetInvalidEmail() throws Exception {
+        // GIVEN
+        String BAD_REQUEST_MESSAGE_FORMAT = "Anfrage ist nicht korrekt.";
+        // WHEN // THEN
+        mockMvc.perform(get("/api/orders/{email}", "123"))
+            .andExpect(status().isBadRequest())
+            .andExpect(result -> assertInstanceOf(IllegalArgumentException.class, result.getResolvedException()))
+            .andExpect(result -> assertEquals(BAD_REQUEST_MESSAGE_FORMAT, result.getResolvedException().getMessage()));
+    }
+
+    @Test
+    void getOrdersByEmail_shouldThrowNotFound_whenNoEntryForEmail() throws Exception {
+        // GIVEN
+        String email = "jon@doe.ioe.io";
+        String ORDERS_NOT_FOUND_MESSAGE_FORMAT = "Bestellungen wurden nicht gefunden.";
+        // WHEN // THEN
+        mockMvc.perform(get("/api/orders/{email}", email))
+            .andExpect(status().isNotFound())
+            .andExpect(result -> assertInstanceOf(EntriesNotFoundException.class, result.getResolvedException()))
+            .andExpect(result -> assertEquals(ORDERS_NOT_FOUND_MESSAGE_FORMAT, result.getResolvedException().getMessage()));
     }
 
     // @Test
