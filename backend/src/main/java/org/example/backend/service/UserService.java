5d4cc0cf45f7e264fd88b7ce1af30d69c1ea22df
git-spec <inkyfischer@gmail.com>
2025-07-07 18:51:32 +0200

    feature: Created UserService.

--- backend/src/main/java/org/example/backend/service/UserService.java
+++ backend/src/main/java/org/example/backend/service/UserService.java
@@ -1,14 +1,55 @@
 package org.example.backend.service;
 
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+
+import com.mongodb.client.result.UpdateResult;
 
 import lombok.RequiredArgsConstructor;
 
-import org.example.backend.repository.UserRepo;
+import org.example.backend.exception.IllegalArgumentException;
+import org.example.backend.exception.DuplicateException;
+import org.example.backend.exception.AccessException;
+import org.example.backend.model.User;
 
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepo userRepo;
+    private final MongoTemplate mongoTemp;
+
+    private static final String ILLEGAL_ARGUMENT = "Angaben fehlen.";
+    private static final String DUPLICATE = "Bereits in der Watchlist enthalten.";
+    private static final String INTERNAL_ERROR = "Es ist ein Fehelr aufgetreten. Versuchen Sie es später noch einmal.";
+
+    public UpdateResult updateWatchlist(
+        String userID, 
+        String productID
+    ) throws IllegalArgumentException, DuplicateException, AccessException {
+        // Checks for null
+        if (userID == null || productID == null) {
+            throw new IllegalArgumentException(ILLEGAL_ARGUMENT);
+        }
+        // Checks for duplicate
+        Query query = new Query(Criteria.where("id").is(userID).and("watchlist").is(productID));
+        boolean exists = mongoTemp.exists(query, User.class);
+        if (exists) {
+            throw new DuplicateException(DUPLICATE);
+        }
+        // Checks result
+        Update update = new Update().addToSet("watchlist", productID);
+        UpdateResult result = mongoTemp.updateFirst(
+            new Query(Criteria.where("id").is(userID)),
+            update,
+            User.class
+        );
+        if (result.wasAcknowledged()) {
+            return result;
+        } else {
+            throw new AccessException(INTERNAL_ERROR);
+        }
+    }
 }
--- backend/src/main/java/org/example/backend/service/WatchlistService.java
+++ backend/src/main/java/org/example/backend/service/WatchlistService.java
@@ -1,5 +1,9 @@
 package org.example.backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -8,9 +12,11 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+import org.example.backend.model.User;
 import org.example.backend.model.WatchlistItem;
 import org.example.backend.model.dto.WatchlistItemDTO;
 import org.example.backend.repository.WatchlistRepo;
+import org.springframework.data.mongodb.core.query.Update;
 
 
 @Service
@@ -19,13 +25,13 @@ public class WatchlistService {
     private final WatchlistRepo watchlistRepo;
     private final IDService idService;
 
-    public WatchlistItem addWatchlistItem(WatchlistItemDTO itemDTO) {
+    // public WatchlistItem addWatchlistItem(WatchlistItemDTO itemDTO) {
 
-        System.out.println("WatchlistItemDTO: " + itemDTO);
+    //     System.out.println("WatchlistItemDTO: " + itemDTO);
 
-        WatchlistItem item = new WatchlistItem(idService.createID(), itemDTO.userEmail(), itemDTO.product());
-        return watchlistRepo.save(item);
-    }
+    //     WatchlistItem item = new WatchlistItem(idService.createID(), itemDTO.userEmail(), itemDTO.product());
+    //     return watchlistRepo.save(item);
+    // }
 
     public List<WatchlistItem> getWatchlistItems(String email) {
         return watchlistRepo.findAllByUserEmail(email);
