33007efc5a8215f8bb3c59395f27bc96d3dec854
git-spec <inkyfischer@gmail.com>
2025-07-09 18:03:31 +0200

    test: Created tests for addUser in UserServiceTest.

--- backend/src/test/java/org/example/backend/service/UserServiceTest.java
+++ backend/src/test/java/org/example/backend/service/UserServiceTest.java
@@ -1,44 +1,105 @@
 package org.example.backend.service;
 
+import java.time.Instant;
 import java.util.List;
 
-import org.example.backend.exception.AccessException;
-import org.example.backend.exception.DuplicateException;
-import org.example.backend.exception.IllegalArgumentException;
-import org.example.backend.model.User;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
-
 import com.mongodb.client.result.UpdateResult;
+
+import org.example.backend.exception.AccessException;
+import org.example.backend.exception.DuplicateException;
+import org.example.backend.exception.IllegalArgumentException;
+import org.example.backend.model.User;
+import org.example.backend.model.dto.UserDTO;
+import org.example.backend.repository.UserRepo;
+
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+    @Mock
+    IDService mockID;
+    @Mock
+    private UserRepo mockRepo;
     @Mock
     private MongoTemplate mockTemp;
     @Mock
     private UpdateResult mockResult;
     @InjectMocks
     private UserService userService;
+    
+    User user = new User("test123", "123@test.com", "Jon", "Doe", Instant.now());
+    UserDTO userDTO = new UserDTO("123@test.com", "Jon", "Doe");
+
+    @Test
+    void addUser_shouldAddUserToDB_whenGetUserDTO() {
+        // GIVEN
+        mockRepo.save(user);
+        // WHEN
+        when(mockID.createID()).thenReturn("test123");
+        when(mockRepo.save(any(User.class))).thenReturn(user);
+        User actual = userService.addUser(userDTO);
+        String id = mockID.createID();
+        // THEN
+        assertEquals(user, actual);
+        assertEquals("test123", id);
+        verify(mockRepo, times(1)).save(user);
+    }
+
+    @Test
+    void addUsert_shouldThrowIllegalArgumentException_whenGetNull() throws IllegalArgumentException {
+        // WHEN
+        UserDTO invalidDTO = new UserDTO(null, "Jon", "Doe");
+        // THEN
+        assertThrows(
+            IllegalArgumentException.class, 
+            () -> userService.addUser(invalidDTO)
+        );
+    }
+
+    @Test
+    void addUser_shouldThrowDuplicateException_whenUserExist() throws DuplicateException {
+        // GIVEN
+        mockRepo.save(user);
+        // WHEN
+        when(mockRepo.findByEmail(user.getEmail())).thenThrow(new DuplicateException("Fehler"));
+        // THEN
+        assertThrows(DuplicateException.class, () -> 
+            userService.addUser(userDTO)
+        );
+    }
+
+    @Test
+    void addUsert_shouldThrowAccessException_onDataAccessException() throws AccessException {
+        // THEN
+        assertThrows(
+            AccessException.class, 
+            () -> userService.addUser(userDTO)
+        );
+    }
 
     @Test
     void updateWathlist_shouldUpdateWatchlist_whenGetUserIdAndProductId() {
         // GIVEN
         UpdateResult updateResult = mock(UpdateResult.class);
-        User user = new User("test123", "123@test.com", "Jon", "Doe", List.of());
+        user.setWatchlist(List.of());
         mockTemp.save(user);
         // WHEN
         when(updateResult.wasAcknowledged()).thenReturn(true);
@@ -71,7 +132,7 @@ public class UserServiceTest {
     }
 
     @Test
-    void updateWatchlist_shouldThrowCustomException_onDataAccessException() throws AccessException {
+    void updateWatchlist_shouldThrowAccessException_onDataAccessException() throws AccessException {
         // WHEN
         when(mockResult.wasAcknowledged()).thenReturn(true);
         when(mockTemp.updateFirst(any(Query.class), any(Update.class), eq(User.class)))
