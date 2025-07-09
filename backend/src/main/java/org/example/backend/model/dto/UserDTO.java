565db3f2c04c135b1a07e02a229af6efc7da3d02
git-spec <inkyfischer@gmail.com>
2025-07-09 18:00:40 +0200

    feature: Created class UserDTO.

---
+++ backend/src/main/java/org/example/backend/model/dto/UserDTO.java
@@ -0,0 +1,51 @@
+package org.example.backend.model.dto;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import org.example.backend.model.Address;
+
+
+@Getter
+@EqualsAndHashCode 
+@Document("users")
+public class UserDTO {
+    private String sub;
+    private final String email; 
+    private String gender;
+    private final String givenName; 
+    private final String familyName;
+    private Address address;
+    private List<String> watchlist;
+
+
+    public UserDTO( 
+        String email, 
+        String givenName, 
+        String familyName
+    ) {
+        this.email = email;
+        this.givenName = givenName;
+        this.familyName = familyName;
+    }
+
+    public void setSub(String sub) {
+        this.sub = sub;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setWatchlist(List<String> watchlist) {
+        this.watchlist = watchlist;
+    }
+}
