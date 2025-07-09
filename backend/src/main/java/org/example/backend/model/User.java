9c01ec0f3aeb82b1a0a2662920464447c7126fc9
git-spec <inkyfischer@gmail.com>
2025-07-09 18:00:28 +0200

    feature: Created class User.

--- backend/src/main/java/org/example/backend/model/User.java
+++ backend/src/main/java/org/example/backend/model/User.java
@@ -4,12 +4,54 @@ import java.util.List;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
+import java.time.Instant;
+
+
+@Getter
+@EqualsAndHashCode 
 @Document("users")
-public record User(
-    String id, 
-    String email, 
-    String firstname, 
-    String lastname,
-    List<String> watchlist
-) {}
+public class User {
+    private final String id;
+    private String sub;
+    private final String email; 
+    private String gender;
+    private final String givenName; 
+    private final String familyName;
+    private Address address;
+    private List<String> watchlist;
+    private final Instant createdAt;
+
+
+    public User(
+        String id, 
+        String email, 
+        String givenName, 
+        String familyName,  
+        Instant createdAt
+    ) {
+        this.id = id;
+        this.email = email;
+        this.givenName = givenName;
+        this.familyName = familyName;
+        this.createdAt = createdAt;
+    }
+
+    public void setSub(String sub) {
+        this.sub = sub;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setWatchlist(List<String> watchlist) {
+        this.watchlist = watchlist;
+    }
+}
