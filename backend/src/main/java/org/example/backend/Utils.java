e50aa30ae3ccea20aca1cec089dbb48a408a48f8
git-spec <inkyfischer@gmail.com>
2025-07-13 22:23:15 +0200

    feature: Added isValidUuidFormat to Utils.

--- backend/src/main/java/org/example/backend/Utils.java
+++ backend/src/main/java/org/example/backend/Utils.java
@@ -6,9 +6,9 @@ import java.util.regex.Pattern;
 public interface Utils {
     static final String EMAIL_REGEX = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
 
-    // public static boolean isValidUuidFormat(String uuid) {
-    //     return uuid != null && uuid.matches("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
-    // }
+    public static boolean isValidUuidFormat(String uuid) {
+        return uuid != null && uuid.matches("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+    }
 
     public static boolean isEmailValid(String email) {
         return Pattern.compile(EMAIL_REGEX)
