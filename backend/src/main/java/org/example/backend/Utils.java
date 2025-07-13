594debfa95ddae5049ee9d86a167acc5e50b6bd2
git-spec <inkyfischer@gmail.com>
2025-07-13 12:12:04 +0200

    feature: Created interface Utils.

---
+++ backend/src/main/java/org/example/backend/Utils.java
@@ -0,0 +1,18 @@
+package org.example.backend;
+
+import java.util.regex.Pattern;
+
+
+public interface Utils {
+    static final String EMAIL_REGEX = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+
+    // public static boolean isValidUuidFormat(String uuid) {
+    //     return uuid != null && uuid.matches("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+    // }
+
+    public static boolean isEmailValid(String email) {
+        return Pattern.compile(EMAIL_REGEX)
+                    .matcher(email)
+                    .matches();
+    }
+}
