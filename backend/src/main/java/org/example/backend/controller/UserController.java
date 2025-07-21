530cea49d203c7cc01ae488f4cb5fcb9501a76e6
git-spec <inkyfischer@gmail.com>
2025-07-21 13:45:17 +0200

    feature: Created updateWatchlist and removeWatchlist in  UserController.

--- backend/src/main/java/org/example/backend/controller/UserController.java
+++ backend/src/main/java/org/example/backend/controller/UserController.java
@@ -2,11 +2,15 @@ package org.example.backend.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.example.backend.model.User;
+import org.example.backend.model.WatchlistItem;
+import org.example.backend.model.dto.UserDTO;
+import org.example.backend.model.dto.WatchlistItemDTO;
+import org.example.backend.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,19 +19,11 @@ import com.mongodb.client.result.UpdateResult;
 
 import lombok.AllArgsConstructor;
 
-import org.example.backend.model.WatchlistItem;
-import org.example.backend.model.User;
-import org.example.backend.model.dto.UserDTO;
-import org.example.backend.model.dto.WatchlistItemDTO;
-import org.example.backend.service.UserService;
-import org.example.backend.service.WatchlistService;
-
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
-    private final WatchlistService watchlistService;
     private final UserService userService;
 
     @GetMapping("/{email}")
@@ -40,26 +36,13 @@ public class UserController {
         return userService.addUser(userDTO);
     }
 
-    @GetMapping("/watchlist/{email}")
-    public List<WatchlistItem> getWatchlistItems(@PathVariable String email) {
-        return watchlistService.getWatchlistItems(email);
-    }
-
-    // @PostMapping("/watchlist")
-    // public WatchlistItem addWatchlistItem(@RequestBody WatchlistItemDTO itemDTO) {
-
-    //     System.out.println("WatchlistItemDTO: " + itemDTO);
-
-    //     return watchlistService.addWatchlistItem(itemDTO);
-    // }
-
-    @DeleteMapping("/watchlist/{id}")
-    public void deleteWatchlistItem(@PathVariable String id) {
-        watchlistService.deleteWatchlistItem(id);
+    @PostMapping("/watchlist")
+    public void deleteWatchlistItem(@RequestBody WatchlistItemDTO itemDTO) {
+        userService.removeWatchlistItem(itemDTO);
     }
 
     @PutMapping("/watchlist")
-    public UpdateResult updateWatchlist(@RequestBody WatchlistItemDTO body) {
-        return userService.updateWatchlist(body.idToken(), body.productID());
+    public UpdateResult updateWatchlist(@RequestBody WatchlistItemDTO itemDTO) {
+        return userService.updateWatchlist(itemDTO);
     }
 } 
